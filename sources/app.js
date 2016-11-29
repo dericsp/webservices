66e703dfccbacbd78914905399ff1575c9d4400b
Eric Henrique Garcia <erichenriquegarcia@MacBook-Pro-de-Eric.local>
2016-11-28 22:50:05 -0200

    nova atualizacao do app.js

--- sources/app.js
+++ sources/app.js
@@ -3,5 +3,19 @@ import express from 'express'
 const server = express()
 const port = process.env.PORT || 3000
 
+server.get('/users', function(req, res) {
+  const users = [
+    {
+      name: 'Eric'
+    }, 
+    {
+      name: 'Clara'
+    },
+  ]
+
+  res.json(users)
+
+})
+
 server.listen(port, () => console.log(`localhost:${port}`))
 
